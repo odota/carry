933dead8becbe70099964fe3bcdf1a36af69a728
Albert Cui <albertqcui@gmail.com>
2018-02-19 07:04:41 +0000

    add webpack

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Legacy Application for donations to the OpenDota project
 
 ## Deployment
 ````
+sudo ./start.sh
+````
+
+## Updates
+````
 git pull origin master
 pm2 restart index
 ````
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # Install node
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && sudo apt-get install -y nodejs
 npm install
+node_modules/webpack/bin/webpack.js build
 PROVIDER=gce ./node_modules/pm2/bin/pm2 start index.js
